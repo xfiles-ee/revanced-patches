fecff212208e6413665f83dc4e3d3a2444fae4e1
xfilesee34 <56469816+xfilesee34@users.noreply.github.com>
2023-08-04 13:56:11 +0300

    Update Constants.kt

--- src/main/kotlin/app/revanced/patches/youtube/misc/microg/shared/Constants.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/microg/shared/Constants.kt
@@ -1,8 +1,8 @@
 package app.revanced.patches.youtube.misc.microg.shared
 
 internal object Constants {
-    const val REVANCED_APP_NAME = "YouTube ReVanced"
-    const val REVANCED_PACKAGE_NAME = "app.revanced.android.youtube"
+    const val REVANCED_APP_NAME = "YouTube Sleep"
+    const val REVANCED_PACKAGE_NAME = "app.youtubesleep.android.youtube"
     const val PACKAGE_NAME = "com.google.android.youtube"
     const val SPOOFED_PACKAGE_NAME = PACKAGE_NAME
     const val SPOOFED_PACKAGE_SIGNATURE = "24bb24c05e47e0aefa68a58a766179d9b613a600"
